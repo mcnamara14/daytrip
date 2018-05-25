ceba03660207b7b00a9ca9331b64e452062586b2
Tyler McNamara <mcnamara14@gmail.com>
2018-05-24 18:37:27 -0600

    Add location input form

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -7,13 +7,33 @@ export class Login extends Component {
     super();
 
     this.state = {
+      location: ''
     }
+  }
+
+  onChangeHandler = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      location: value
+    })
   }
 
   render() {
     return (
       <div className="Login">
-        I'm a login form
+        <h1>Sign up to get started</h1>
+        <p>Why must they do that inspect anything brought into the house, yet 
+            destroy couch as revenge. Sleep on my human's head find me of your food meh.</p>
+        <section className="loginForm">
+        <h2>Choose your location</h2>
+          <input
+            name='location'
+            value={this.state.location}
+            onChange={this.onChangeHandler}
+            placeholder='Denver, CO'
+          />
+        </section>
       </div>
     );
   }
