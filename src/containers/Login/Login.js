b696305198c204c386311507f8d9035f791ee515
Tyler McNamara <mcnamara14@gmail.com>
2018-05-24 18:29:06 -0600

    Setup Login container files

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+
+export class Login extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        I'm a login form
+      </div>
+    );
+  }
+}
+
+export default Login;
