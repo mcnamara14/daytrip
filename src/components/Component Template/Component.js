81a5b255d49e1021afc32fb9806fa8a445558575
Tyler McNamara <mcnamara14@gmail.com>
2018-05-24 18:20:14 -0600

    Setup Header component files

---
+++ src/components/Component Template/Component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Component.css';
+
+export const Component = () =>  {
+  return (
+    <div className="Component">
+    </div>
+  );
+}
+export default Component;
