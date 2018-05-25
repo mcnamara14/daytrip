a0a7e6c1fb4aefda791519324256466d6ee4718f
Tyler McNamara <mcnamara14@gmail.com>
2018-05-24 21:40:34 -0600

    Add login icon and complete initial header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import headerLogo from './assets/header-logo.jpg';
+import loginIcon from './assets/header-login-icon.jpg';
 
 export const Header = () =>  {
   return (
@@ -11,7 +12,10 @@ export const Header = () =>  {
         <NavLink to='/events'>Events</NavLink>
         <NavLink to='/trips'>Trips</NavLink>
         <NavLink to='/account'>Account</NavLink>
-        <NavLink to='/login' className="navLogin">Login</NavLink>
+        <div className='loginLink'>
+          <img src={loginIcon} className="loginIcon" />
+          <NavLink to='/login' className="navLogin">Login</NavLink>
+        </div>
       </nav>
     </header>
   );
