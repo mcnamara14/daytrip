0b74e24fa620739b55289833be94a7ee1166d53f
Tyler McNamara <mcnamara14@gmail.com>
2018-05-24 21:21:32 -0600

    Add header links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
+import headerLogo from './assets/header-logo.jpg';
 
 export const Header = () =>  {
   return (
     <header>  
-      I'm a header
+      <img src={headerLogo} className="headerLogo" />
+      <nav>
+        <NavLink to='/events'>Events</NavLink>
+        <NavLink to='/trips'>Trips</NavLink>
+        <NavLink to='/account'>Account</NavLink>
+        <NavLink to='/login' className="navLogin">Login</NavLink>
+      </nav>
     </header>
   );
 }
