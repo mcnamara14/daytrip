75f9b687fdab81f6f4c0ce68d97045ac29309ccd
Tyler McNamara <mcnamara14@gmail.com>
2018-05-24 18:45:50 -0600

    Add email and password inputs

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -7,16 +7,22 @@ export class Login extends Component {
     super();
 
     this.state = {
-      location: ''
+      location: '',
+      email: '',
+      password: ''
     }
   }
 
   onChangeHandler = (event) => {
-    const { value } = event.target;
+    const { name, value } = event.target;
 
     this.setState({
-      location: value
+      [name]: value
     })
+  }
+
+  onClickHandler = () => {
+
   }
 
   render() {
@@ -25,14 +31,32 @@ export class Login extends Component {
         <h1>Sign up to get started</h1>
         <p>Why must they do that inspect anything brought into the house, yet 
             destroy couch as revenge. Sleep on my human's head find me of your food meh.</p>
-        <section className="loginForm">
-        <h2>Choose your location</h2>
-          <input
-            name='location'
-            value={this.state.location}
-            onChange={this.onChangeHandler}
-            placeholder='Denver, CO'
-          />
+        <section className="signupForm">
+          <h2>Choose your location</h2>
+          <form onClick={this.onClickHandler} className="locationInput" >
+            <input
+              name='location'
+              value={this.state.location}
+              onChange={this.onChangeHandler}
+              placeholder='Denver, CO'
+            />
+          </form>
+          <section className="signupOptions">
+            <article className="emailSignup">
+              <input
+                name='email'
+                value={this.state.email}
+                onChange={this.onChangeHandler}
+                placeholder='ex. tyler@daytrips.com'
+              />
+              <input
+                name='password'
+                value={this.state.location}
+                onChange={this.onChangeHandler}
+                placeholder='Enter a password'
+              />
+            </article>
+          </section>
         </section>
       </div>
     );
