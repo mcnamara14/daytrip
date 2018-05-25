b7cc01b3fa9866faf3d846a623c95dd28b293c10
Tyler McNamara <mcnamara14@gmail.com>
2018-05-24 20:53:54 -0600

    Update gitignore

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBhhPB1euEwuA3tbCshgSZafio8mkU63Hc",
-    authDomain: "daytrip-7d5c4.firebaseapp.com",
-    databaseURL: "https://daytrip-7d5c4.firebaseio.com",
-    projectId: "daytrip-7d5c4",
-    storageBucket: "daytrip-7d5c4.appspot.com",
-    messagingSenderId: "1096579022176",
+
   };
 
 if (!firebase.apps.length) {
