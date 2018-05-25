a253f3f94eaa943e6dd56f8c04e4d318036e3913
Tyler McNamara <mcnamara14@gmail.com>
2018-05-24 20:59:25 -0600

    Update apiKey variable to hide firebase key from the public

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,13 @@
 import * as firebase from 'firebase';
+import { firebaseApiKey } from './apiKey';
 
 const config = {
-test
+  apiKey: firebaseApiKey,
+  authDomain: "daytrip-c5c3a.firebaseapp.com",
+  databaseURL: "https://daytrip-c5c3a.firebaseio.com",
+  projectId: "daytrip-c5c3a",
+  storageBucket: "daytrip-c5c3a.appspot.com",
+  messagingSenderId: "532630776936",
   };
 
 if (!firebase.apps.length) {
