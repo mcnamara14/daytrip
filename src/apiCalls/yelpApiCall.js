49274782a729b40caf5ac49997b9ab00ea6bfd28
Tyler McNamara <mcnamara14@gmail.com>
2018-05-27 16:47:39 -0600

    Test fetch call with rating and categories filters

--- src/apiCalls/yelpApiCall.js
+++ src/apiCalls/yelpApiCall.js
@@ -4,7 +4,7 @@ export const yelpApiCall = async (locationLat, locationLong) => {
   let headers = new Headers();
   headers.append("Authorization", "Bearer " + yelpApiKey);
 
-  const result = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?&latitude=${locationLat}&longitude=${locationLong}`, {
+  const result = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?&categories=beerbar,hotel_bar,irish_pubs,lounges,pubs,speakeasies,sportsbars,whiskeybars&latitude=${locationLat}&longitude=${locationLong}&radius=1000&price=1&sort_by=rating`, {
     headers
   });
   const data = await result.json();
