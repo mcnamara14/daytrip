2044b3432728cb79fb8f518cac7308e29302df01
Tyler McNamara <mcnamara14@gmail.com>
2018-05-30 12:00:21 -0600

    Add / remove price ranges from state when selected

--- src/containers/StopSelection/StopSelection.js
+++ src/containers/StopSelection/StopSelection.js
@@ -9,7 +9,8 @@ export class StopSelection extends Component {
     super();
 
     this.state = {
-      selectedOption: {}
+      selectedOption: {},
+      priceRanges:[]
     }
   };
 
@@ -29,8 +30,24 @@ onSelect = (selectedOption) => {
   }
 }
 
+changePriceRange = (price) => {
+  if (!this.state.priceRanges.includes(price)) {
+    const priceRanges = [...this.state.priceRanges, price]
+    this.setState({
+      priceRanges 
+    })
+  } else {
+    const priceRanges = this.state.priceRanges.filter(range => {
+      return range !== price;
+    })
+    this.setState({
+      priceRanges 
+    })
+  }
+}
+
 render() {
-  const { selectedOption} = this.state;
+  const { selectedOption, priceRange } = this.state;
   const beforeAfter = this.props.type === 'before' ? 'before' : 'after';
   const restaurantBar = this.props.type === 'before' ? 'restaurant' : 'bar';
   const beforeAfterCategories = this.props.type === 'before' ? this.beforeEventCategories : this.afterEventCategories;
@@ -49,10 +66,10 @@ render() {
       />
       <p className="filterTitle">Price</p>
       <div className="priceRange">
-        <span>$</span>
-        <span>$$</span>
-        <span>$$$</span>
-        <span className="last">$$$$</span>
+        <span onClick={() => this.changePriceRange('1')} >$</span>
+        <span onClick={() => this.changePriceRange('2')} >$$</span>
+        <span onClick={() => this.changePriceRange('3')} >$$$</span>
+        <span onClick={() => this.changePriceRange('4')} className="last">$$$$</span>
       </div>
       <button>Submit</button>
     </div>
