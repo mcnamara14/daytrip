e8483947304a68eecd8ca6dad6b8e49154be8c5c
Tyler McNamara <mcnamara14@gmail.com>
2018-05-30 22:14:43 -0600

    Render restaurants with basic styling

--- src/containers/Plan/Plan.js
+++ src/containers/Plan/Plan.js
@@ -4,10 +4,19 @@ import { connect } from 'react-redux';
 export class Plan extends Component {
 
   render() {
+    const { name, rating, address, price, reviews, image, index } = this.props;
+    const backgroundImage = {backgroundImage: "url(" + image + ")"};
+    const className = `restaurant${index}`;
+
     return (
-      <div>
-        <h2>{this.props.name}</h2>
-      </div>
+      <article className={className}>
+        <h2>{name}</h2>
+        <p className="rating">{rating}</p>
+        <p className="reviewCount">{reviews}</p>
+        <p className="price">{price}</p>
+        <p className="address">{address}</p>
+        <div className="restaurantImage" style={backgroundImage}></div>
+      </article>
     )
   }
 }
--- src/containers/Plans/Plans.js
+++ src/containers/Plans/Plans.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Plan from '../Plan/Plan';
+import './Plans.css';
 
 export class Plans extends Component {
-
   render () {
-    const plans = this.props.suggestedRestaurants.map(restaurant => {
+    const plans = this.props.suggestedRestaurants.map((restaurant, index) => {
       const { name, rating, address, price, review_count, image_url } = restaurant;
 
-      return <Plan name={name} />
+      return <Plan name={name} rating={rating} address={address} price={price} reviews={review_count} image={image_url} index={index}/>
     })
     
     return (
-      <div className="plans">
-        { plans }
-      </div>
+      <section className="suggestedPlans">
+        <div className="plansContainer">
+          { plans }
+        </div>
+      </section>
     )
   }
 }
