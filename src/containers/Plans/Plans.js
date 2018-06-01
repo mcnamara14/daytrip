4cbc2c537e140df58f527a88cbaed79dc50c8c5a
Tyler McNamara <mcnamara14@gmail.com>
2018-06-01 15:36:57 -0600

    Add select plan button

--- src/containers/Plans/Plans.js
+++ src/containers/Plans/Plans.js
@@ -10,15 +10,20 @@ export class Plans extends Component {
       const { name, rating, address, price, review_count, image_url } = restaurant;
       const { title, image, venue, date } = this.props.selectedEvent;
       const eventPrice = this.props.selectedEvent.price;
+      const planNum = index + 1;
 
       return (
-        <section className="plan" key={index}>
-          <Plan title={name} rating={rating} location={address} price={price} reviews={review_count} image={image_url} type={'restaurant'} index={index}/>
-          <img src={require('./assets/route-img.jpg')} className="routeImg" />
-          <Plan title={title} rating={''} location={venue} price={eventPrice} reviews={date} image={image} type={'event'} index={index}/>
-          <img src={require('./assets/route-img.jpg')} className="routeImg" />
-          <Plan title={name} rating={rating} location={address} price={price} reviews={review_count} image={image_url} type={'restaurant'} index={index}/>
-        </section>
+        <div key={index} className="planContainer">
+          <h4>Plan {planNum}</h4>
+          <section className="plan">
+            <Plan title={name} rating={rating} location={address} price={price} reviews={review_count} image={image_url} type={'restaurant'} index={planNum}/>
+            <img src={require('./assets/route-img.jpg')} className="routeImg" />
+            <Plan title={title} rating={''} location={venue} price={eventPrice} reviews={date} image={image} type={'event'} index={planNum}/>
+            <img src={require('./assets/route-img.jpg')} className="routeImg" />
+            <Plan title={name} rating={rating} location={address} price={price} reviews={review_count} image={image_url} type={'restaurant'} index={planNum}/>
+          </section>
+          <button>Select Plan</button>
+        </div>
       )
     })
     
