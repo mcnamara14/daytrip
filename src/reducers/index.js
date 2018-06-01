f098191e6943c2e2358507eeb65e7ec3fac2800b
Tyler McNamara <mcnamara14@gmail.com>
2018-06-01 15:53:23 -0600

    Add selected bars to the store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,13 @@ import { user } from './user';
 import { recentEvents } from './recentEvents';
 import { selectedEvent } from './selectedEvent';
 import { suggestedRestaurants } from './suggestedRestaurants';
+import { suggestedBars } from './suggestedBars';
+
 
 export const rootReducer = combineReducers({
   user,
   recentEvents, 
   selectedEvent,
-  suggestedRestaurants
+  suggestedRestaurants,
+  suggestedBars
 })
