bcceb725657da15625e3415aad58f4b0a961a8b9
Tyler McNamara <mcnamara14@gmail.com>
2018-06-01 11:10:53 -0600

    Add selected event to page without styling

--- src/containers/Plan/Plan.js
+++ src/containers/Plan/Plan.js
@@ -4,17 +4,17 @@ import { connect } from 'react-redux';
 export class Plan extends Component {
 
   render() {
-    const { name, rating, address, price, reviews, image, index } = this.props;
+    const { title, rating, location, price, reviews, image, index } = this.props;
     const backgroundImage = {backgroundImage: "url(" + image + ")"};
     const className = `restaurant${index}`;
 
     return (
       <article className={className}  >
-        <h2>{name}</h2>
+        <h2>{title}</h2>
         <p className="rating">{rating}</p>
         <p className="reviewCount">{reviews}</p>
         <p className="price">{price}</p>
-        <p className="address">{address}</p>
+        <p className="location">{location}</p>
         <div className="restaurantImage" style={backgroundImage}></div>
       </article>
     )
--- src/containers/Plans/Plans.js
+++ src/containers/Plans/Plans.js
@@ -4,11 +4,18 @@ import Plan from '../Plan/Plan';
 import './Plans.css';
 
 export class Plans extends Component {
+
   render () {
     const plans = this.props.suggestedRestaurants.map((restaurant, index) => {
       const { name, rating, address, price, review_count, image_url } = restaurant;
+      const { title, image, eventPrice, venue, date } = this.props.selectedEvent;
 
-      return <Plan name={name} rating={rating} address={address} price={price} reviews={review_count} image={image_url} index={index}/>
+      return (
+        <section className="plan">
+          <Plan title={title} rating={''} location={venue} price={eventPrice} reviews={date} image={image} index={index}/>
+          <Plan title={name} rating={rating} location={address} price={price} reviews={review_count} image={image_url} index={index}/>
+          </section>
+      )
     })
     
     return (
@@ -22,7 +29,8 @@ export class Plans extends Component {
 }
 
 export const mapStateToProps = (state) => ({
-  suggestedRestaurants: state.suggestedRestaurants
+  suggestedRestaurants: state.suggestedRestaurants,
+  selectedEvent: state.selectedEvent
 })
 
 export default connect(mapStateToProps)(Plans)
