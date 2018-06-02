320eeafde99cc224654f2d1d83830c91e5edc41a
Tyler McNamara <mcnamara14@gmail.com>
2018-06-02 15:16:46 -0600

    Add getPlans testing, two remaining

--- src/containers/Plans/Plans.test.js
+++ src/containers/Plans/Plans.test.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Plans } from './Plans';
+import { Stop } from '../Stop/Stop';
 import { shallow } from 'enzyme';
 import { mockCleanRestaurantAndBar, mockDirtyRecentEvents } from '../../mockData';
 
-it('should match the snapshot', () => {
-  const mockSuggestedBarAndRestaurantData = mockCleanRestaurantAndBar;
-  const mockSelectedEvent = mockDirtyRecentEvents.events;
+describe('Plans', () => {
+  let wrapper;
 
-  const wrapper = shallow(<Plans 
-    suggestedBars={mockSuggestedBarAndRestaurantData}
-    suggestedRestaurants={mockSuggestedBarAndRestaurantData} 
-    selectedEvent={mockSelectedEvent}
-    />);
+  beforeEach(() => {
+    const mockSuggestedBarAndRestaurantData = mockCleanRestaurantAndBar;
+    const mockSelectedEvent = mockDirtyRecentEvents.events;
+  
+    wrapper = shallow(<Plans 
+      suggestedBars={mockSuggestedBarAndRestaurantData}
+      suggestedRestaurants={mockSuggestedBarAndRestaurantData} 
+      selectedEvent={mockSelectedEvent}
+      />);
+  
+  })
 
-  expect(wrapper).toMatchSnapshot();
-});
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should returns plans when there are suggested bars and restaurants', () => { 
+    wrapper.instance().getPlans();
+
+    expect(wrapper.find('.planContainer').length).toEqual(1);
+    expect(wrapper.find('Stop').length).toEqual(3);
+  });
+})
+
