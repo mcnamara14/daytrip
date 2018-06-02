0809831b75073b140695323c5ca63735322c1b3f
Tyler McNamara <mcnamara14@gmail.com>
2018-06-02 15:52:16 -0600

    Add snapshot test

--- src/containers/Stop/Stop.test.js
+++ src/containers/Stop/Stop.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Plans } from './Plans';
+import { Stop } from './Stop';
 import { shallow } from 'enzyme';
 
 it('should match the snapshot', () => {
-
-
-  const wrapper = shallow(<Plans />);
+  const wrapper = shallow(<Stop />);
 
   expect(wrapper).toMatchSnapshot();
 });
