700bcb62ba6fea53326d13e51dbf892a6ecbc55f
Tyler McNamara <mcnamara14@gmail.com>
2018-06-02 14:55:25 -0600

    Add snapshot test

--- src/containers/Plans/Plans.test.js
+++ src/containers/Plans/Plans.test.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Plans } from './Plans';
 import { shallow } from 'enzyme';
+import { mockCleanRestaurantAndBar, mockDirtyRecentEvents } from '../../mockData';
 
 it('should match the snapshot', () => {
+  const mockSuggestedBarAndRestaurantData = mockCleanRestaurantAndBar;
+  const mockSelectedEvent = mockDirtyRecentEvents.events;
 
-
-  const wrapper = shallow(<Plans />);
+  const wrapper = shallow(<Plans 
+    suggestedBars={mockSuggestedBarAndRestaurantData}
+    suggestedRestaurants={mockSuggestedBarAndRestaurantData} 
+    selectedEvent={mockSelectedEvent}
+    />);
 
   expect(wrapper).toMatchSnapshot();
 });
