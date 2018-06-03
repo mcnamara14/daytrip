b16d499cc78e47f6cdfbb1ff4e761c50d4e77b36
Tyler McNamara <mcnamara14@gmail.com>
2018-06-03 16:38:05 -0600

    Add onSelect test

--- src/containers/StopSelection/StopSelection.test.js
+++ src/containers/StopSelection/StopSelection.test.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { StopSelection } from './StopSelection';
 import { shallow } from 'enzyme';
+import { mockCleanRestaurantAndBar } from '../../mockData';
 
+describe('StopSelection', () => {
 it('should match the snapshot', () => {
   const wrapper = shallow(<StopSelection />)
 
   expect(wrapper).toMatchSnapshot();
 });
+
+  describe('onSelect', () => {
+    it('should set state to selected option', () => {
+      const wrapper = shallow(<StopSelection />)
+
+      wrapper.instance().onSelect(mockCleanRestaurantAndBar)
+     
+      expect(wrapper.state('selectedOption')).toEqual(mockCleanRestaurantAndBar)
+    });
+    
+  })
+
+})
