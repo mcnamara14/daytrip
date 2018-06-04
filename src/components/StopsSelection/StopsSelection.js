fc0de10a1b73c1c1b8d396324b0a999832e94954
Tyler McNamara <mcnamara14@gmail.com>
2018-06-04 15:37:13 -0600

    Move storing of restaurants and bars to store to StopsContainer

--- src/components/StopsSelection/StopsSelection.js
+++ src/components/StopsSelection/StopsSelection.js
@@ -2,30 +2,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './StopsSelection.css';
 import StopSelection from '../../containers/StopSelection/StopSelection';
+import { storeSuggestedRestaurants, storeSuggestedBars } from '../../actions';
+import { fetchRestaurantsAndBars } from '../../apiCalls';
 
 export class StopsSelection extends Component {
-  // storeRestaurantsOrBars = async () => {
-  //   const { 
-  //     selectedEvent, 
-  //     type, 
-  //     storeSuggestedRestaurants, 
-  //     storeSuggestedBars 
-  //   } = this.props;
-  //   const { selectedOption, priceRanges } = this.state;
-   
-  //   if (selectedEvent !== null && selectedOption !== null) {
-  //     const latitude = selectedEvent.latitude;
-  //     const longitude = selectedEvent.longitude;
-  //     const price = priceRanges.sort().join();
-  //     const category = selectedOption.alias;
-  //     const suggestedRestaurantsBars = 
-  //       await fetchRestaurantsAndBars(latitude, longitude, price, category);
-  //     type === 'before' ? storeSuggestedRestaurants(suggestedRestaurantsBars) : 
-  //       storeSuggestedBars(suggestedRestaurantsBars);
-  //   } else if (selectedEvent === null) {
-  //     this.toggleEventError();
-  //   }
-  // }
+  storeRestaurantsAndBars = () => {
+    const { 
+      selectedEvent,
+      restaurantFilters,
+      barFilters
+    } = this.props;
+
+    if (restaurantFilters.category !== undefined && barFilters.category !== undefined ) {
+      const latitude = selectedEvent.latitude;
+      const longitude = selectedEvent.longitude;
+      this.storeSuggestedRestaurants(latitude, longitude);
+    } else if (selectedEvent === null) {
+      this.toggleEventError();
+    }
+  }
+
+  storeSuggestedRestaurants = async (latitude, longitude) => {
+    const {
+      restaurantFilters,
+      storeSuggestedRestaurants
+    } = this.props;
+
+    const price = restaurantFilters.priceRange.sort().join();
+    const category = restaurantFilters.category;
+    const suggestedRestaurants = 
+      await fetchRestaurantsAndBars(latitude, longitude, price, category);
+    storeSuggestedRestaurants(suggestedRestaurants);
+  }
+
+  storeSuggestedBars = async (latitude, longitude) => {
+    const {
+      barFilters,
+      storeSuggestedBars
+    } = this.props;
+
+    const price = barFilters.priceRange.sort().join();
+    const category = barFilters.category;
+    const suggestedBars = 
+      await fetchRestaurantsAndBars(latitude, longitude, price, category);
+    storeSuggestedBars(suggestedBars);
+  }
 
   render() {
     return (
@@ -33,15 +54,23 @@ export class StopsSelection extends Component {
         <StopSelection type={'before'}/>
         <hr />
         <StopSelection type={'after'}/>
-        <button>Add filters</button>
+        <button onClick={this.storeRestaurantsAndBars} >Add filters</button>
       </div>
     );
   }
 }
 
-// export const mapStateToProps = (state) => ({
-//   selectedEvent: state.selectedEvent,
-//   eventError: state.eventError
-// });
+export const mapDispatchToProps = (dispatch) => ({
+  storeSuggestedRestaurants: (restaurants) => {
+    return dispatch(storeSuggestedRestaurants(restaurants));
+  },
+  storeSuggestedBars: (bars) => dispatch(storeSuggestedBars(bars))
+});
 
-// export default connect(mapStateToProps)(StopsSelection);
+export const mapStateToProps = (state) => ({
+  selectedEvent: state.selectedEvent,
+  restaurantFilters: state.restaurantFilters,
+  barFilters: state.barFilters
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StopsSelection);
