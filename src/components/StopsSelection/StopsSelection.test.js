05e45fb5a4183c569b7a02f687ab8a9f246f3401
Tyler McNamara <mcnamara14@gmail.com>
2018-06-05 21:15:51 -0600

    Add storeRestaurantsAndBars tests

--- src/components/StopsSelection/StopsSelection.test.js
+++ src/components/StopsSelection/StopsSelection.test.js
@@ -3,8 +3,101 @@ import ReactDOM from 'react-dom';
 import { StopsSelection } from './StopsSelection';
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<StopsSelection />);
+describe('stopsSelection', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<StopsSelection />);
+  
+    expect(wrapper).toMatchSnapshot();
+  });
 
-  expect(wrapper).toMatchSnapshot();
-});
+  describe('storeRestaurantsAndBars', () => {
+    let wrapper;
+    let mockEvent;
+    let mockRestaurantFilters;
+    let mockBarFilters;
+
+    beforeEach(() => {
+      mockEvent = {
+        latitude: '1234',
+        longitude: '6789',
+        title: 'T-Swift'
+      }
+      mockRestaurantFilters = {
+        category: 'newamerican'
+      }
+      mockBarFilters = {
+        category: 'divebar'
+      }
+
+      wrapper = shallow(<StopsSelection 
+        selectedEvent={mockEvent} 
+        restaurantFilters={mockRestaurantFilters} 
+        barFilters={mockBarFilters} />);
+
+      wrapper.instance().storeSuggestedRestaurants = jest.fn();
+      wrapper.instance().storeSuggestedBars = jest.fn();
+      wrapper.instance().toggleEventError = jest.fn();
+      wrapper.instance().toggleFiltersError= jest.fn();
+    })
+
+    it('should call storeSuggestedRestaurants with the correct args when there are restaraunts and bars', () => {
+      wrapper.instance().storeRestaurantsAndBars();
+
+      const expected = ["1234", "6789"];
+
+      expect(wrapper.instance().storeSuggestedRestaurants).toHaveBeenCalledWith(...expected)
+    })
+
+    it('should call storeSuggestedBars with the correct args when there are restaraunts and bars', () => {
+      wrapper.instance().storeRestaurantsAndBars();
+
+      const expected = ["1234", "6789"];
+
+      expect(wrapper.instance().storeSuggestedBars).toHaveBeenCalledWith(...expected)
+    })
+
+    it('should call toggleEventError when no event is selected', () => {
+      mockEvent = null
+
+      wrapper = shallow(<StopsSelection 
+        selectedEvent={mockEvent} 
+        restaurantFilters={mockRestaurantFilters} 
+        barFilters={mockBarFilters} />);
+
+      wrapper.instance().storeSuggestedRestaurants = jest.fn();
+      wrapper.instance().storeSuggestedBars = jest.fn();
+      wrapper.instance().toggleEventError = jest.fn();
+      wrapper.instance().toggleFiltersError= jest.fn();
+      
+      wrapper.instance().storeRestaurantsAndBars();
+
+      const expected = ["1234", "6789"];
+
+      expect(wrapper.instance().toggleEventError).toHaveBeenCalled()
+    })
+
+    it('should call toggleFiltersError when filters are undefined', () => {
+      mockEvent = null;
+      mockRestaurantFilters = {
+        category: undefined
+      }
+
+      wrapper = shallow(<StopsSelection 
+        selectedEvent={mockEvent} 
+        restaurantFilters={mockRestaurantFilters} 
+        barFilters={mockBarFilters} />);
+
+      wrapper.instance().storeSuggestedRestaurants = jest.fn();
+      wrapper.instance().storeSuggestedBars = jest.fn();
+      wrapper.instance().toggleEventError = jest.fn();
+      wrapper.instance().toggleFiltersError= jest.fn();
+      
+      wrapper.instance().storeRestaurantsAndBars();
+
+      const expected = ["1234", "6789"];
+
+      expect(wrapper.instance().toggleEventError).toHaveBeenCalled()
+    })
+  })
+})
+
